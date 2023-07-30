3436f5957abbeb21e6651b6b75c88a97b3636040
ArtemLevin <python_study_2022@mail.ru>
2023-07-31 01:14:52 +0300

    climbing stairs

--- LeetCode_70.py
+++ LeetCode_70.py
@@ -3,16 +3,20 @@
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 import copy
 
+
 def climbStairs(n):
-    if len(n) == 1 or len(n) == 0:
-        return "nothing to remove"
-    else:
-        m = copy.copy(n)
-        n.pop(0)
-        del m[0:2]
-        climbStairs(n)
-        climbStairs(m)
+    memo = {}
+    memo[1] = 1
+    memo[2] = 2
+
+    def climb(n):
+        if n in memo:
+            return memo[n]
+        else:
+            memo[n] = climb(n - 1) + climb(n - 2)
+            return memo[n]
+
+    return climb(n)
 
 
-
-print(climbStairs([1,1,1,1]))
+print(climbStairs(4))
